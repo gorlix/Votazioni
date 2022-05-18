ad26aa5235736e66d5f18c079a5b8f16144f73d5
BeastOfShadow <57445571+SimoneNegro@users.noreply.github.com>
2022-05-18 10:44:51 +0200

    Creata cartella per lo stile Impostazione per le pagine con la sidebar

--- php/votazione.php
+++ php/votazione.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../stile/style.css">
     <title>Votazione</title>
 </head>
 <body>
-    
+    <div class="header">hola
+        <div class="box-logo"></div>
+        <div class="box-titolo-header"></div>
+    </div>
+    <div clas="sidebar"></div>
+    <div class="contenuti"></div>
 </body>
 </html>
