ca3262414b94cff72215a11ece8c2f1d7bce932a
GabrieleGroppo03 <gabriele.groppo@alessandrinimainardi.edu.it>
2022-05-19 09:04:20 +0200

    Aggiunta shared + conn DB
    Aggiunta shared function e metodi provvisori per la connesisone al database

--- php/login.php
+++ php/login.php
@@ -2,21 +2,50 @@
 <head>
     <title>Login</title>
     <?php
+    require DIR . '/SharedFunction.php';
+    ?>
+    <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail = $_POST['mail'];
         $password = $_POST['password'];
         $password = hash_password($password);
-        if (check_login($mail, $password)) {
-            header("Location: home.php");
-        } else {
-            echo "Wrong mail or password!";
-        }
+        if(valida_mail($mail)){
+            if (check_login(connettiDb(), $mail, $password)) {
+                header("Location: home.php");
+            } else {
+                echo "Wrong mail or password!";
+            }
+        }else
+            echo "Wrong mail format!";
     }
     function hash_password($password) {
         return sha1($password);
     }
-    function check_login($mail, $password) {
-        return ($mail == "admin" && $password == hash_password("admin"));
+    function connettiDb(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "votazioniScolastiche";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        return $conn;
+    }
+    function valida_mail($mail) {
+        $valida = true;
+        if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+            $valida = false;
+        return $valida;
+    }
+    function check_login($conn, $mail, $password){
+        $sql = "SELECT * FROM utente WHERE mail = '$mail' AND pw = '$password'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
     ?>
 </head>
