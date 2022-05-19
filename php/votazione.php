ac86d8f63989151c780d7f690025d24e25202608
BeastOfShadow <57445571+SimoneNegro@users.noreply.github.com>
2022-05-19 08:22:58 +0200

    Creato stile header

--- php/votazione.php
+++ php/votazione.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../stile/style.css">
+    <link rel="stylesheet" href="../stile/globalStyle.css">
     <title>Votazione</title>
 </head>
 <body>
     <div class="header">
-        <div class="box-logo"></div>
-        <div class="box-titolo-header"></div>
+        <div class="box-logo">
+            <img src="../immagini//logoScuola.png" alt="logo scuola" class="logo-scuola">
+        </div>
+        <div class="box-titolo-header">
+            <p class="titolo-header">Votazioni</p>
+        </div>
     </div>
     <div clas="sidebar"></div>
     <div class="contenuti"></div>
