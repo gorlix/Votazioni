48a290cd4e2011ec2ac7fac98e0960ad1d7e2331
Alessandro Gorla <alessandro.gorla@alessandrinimainardi.edu.it>
2022-05-19 12:09:01 +0200

    Modifica Navbar dinamica

--- php/Navbar.php
+++ php/Navbar.php
@@ -1,21 +1,45 @@
 <?php
-    echo
-        "<div class='sidebar'>
-            <ul>
-                <li class='elenco' id='0'>
-                    <a href='gestisci_votazioni.php' class='link'>Gestisci votazioni</a>
-                </li>
-                <li class='elenco' id='1'>
-                    <a href='gestisci_utente.php' class='link'>Gestisci utenti</a>
-                </li>
-                <li class='elenco' id='2'>
-                    <a href='gestisci_gruppo.php' class='link'>Gestisci gruppi</a>
-                </li>
-                <li class='elenco' id='3'>
-                    <a href='home.php' class='link'>Home</a>
-                </li>
-            </ul>
-        </div>";
+    $user = $_SESSION["Id_Utente"];
+    if(!isset($user))
+    {
+        die;
+    }
 
+    echo
+        "<div class='sidebar'>".
+            "<ul>";
+        if()
+        {
+            echo
+                "<li class='elenco' id='0'>" .
+                "<a href='gestisci_votazioni.php' class='link'>Gestisci votazioni</a>" .
+                "</li>";
+        }
+
+        if()
+        {
+            echo
+                "<li class='elenco' id='1'>" .
+                "<a href='gestisci_utente.php' class='link'>Gestisci utenti</a>" .
+                "</li>" ;
+        }
+            if()
+            {
+                echo
+                    "<li class='elenco' id='2'>" .
+                    "<a href='gestisci_gruppo.php' class='link'>Gestisci gruppi</a>" .
+                    "</li>" ;
+            }
+            if()
+            {
+                echo
+                    "<li class='elenco' id='3'>" .
+                    "<a href='home.php' class='link'>Home</a>" .
+                    "</li>" ;
+            }
+
+    echo
+            "</ul>" .
+        "</div>";
 ?>
 
