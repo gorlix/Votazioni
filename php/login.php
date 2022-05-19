13e831746b8df7abd2a5cebc72cb30140605236a
GabrieleGroppo03 <gabriele.groppo@alessandrinimainardi.edu.it>
2022-05-19 09:14:59 +0200

    Update login.php
    spostato hash in linea nella funzione check_login

--- php/login.php
+++ php/login.php
@@ -2,13 +2,12 @@
 <head>
     <title>Login</title>
     <?php
-    require DIR . '/SharedFunction.php';
+    require DIR . '/SharedFunctions.php';
     ?>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail = $_POST['mail'];
         $password = $_POST['password'];
-        $password = hash_password($password);
         if(valida_mail($mail)){
             if (check_login(connettiDb(), $mail, $password)) {
                 header("Location: home.php");
@@ -17,35 +16,6 @@
             }
         }else
             echo "Wrong mail format!";
-    }
-    function hash_password($password) {
-        return sha1($password);
-    }
-    function connettiDb(){
-        $servername = "localhost";
-        $username = "root";
-        $password = "root";
-        $dbname = "votazioniScolastiche";
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        return $conn;
-    }
-    function valida_mail($mail) {
-        $valida = true;
-        if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
-            $valida = false;
-        return $valida;
-    }
-    function check_login($conn, $mail, $password){
-        $sql = "SELECT * FROM utente WHERE mail = '$mail' AND pw = '$password'";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            return true;
-        } else {
-            return false;
-        }
     }
     ?>
 </head>
