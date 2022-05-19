afbb6ee8193aeced4da63c5bad2970aa29821a23
toneskater <toneskater@yahoo.it>
2022-05-19 08:09:11 +0200

    pagina rilancio a login.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+	<!-- FORM PER L'INSERIMENTO DELLE CREDENZIALI
+nome File: formAccesso.html -->
+
+<html>
+	<head>
+		<title> Index </title>
+	</head>
+	<body>
+		<?php header("Location: php/login.php")?>
+	</body>
+</html>
