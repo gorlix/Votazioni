297f7c2c99ed31d591516fa3def3fea9559a9fab
Gabriele Groppo <gabriele.groppo@alessandrinimainardi.edu.it>
2022-05-30 12:33:00 +0200

    Creazione blocchi pagina per
    Getsione utente
    gestione gruppi utente
    Creazione nuovo utente
    Visualizzaizone gruppi in cui l'utente è già inserito

--- php/gestisci_utente.php
+++ php/gestisci_utente.php
@@ -20,7 +20,82 @@
     include "Navbar.php";
     ?>
     <div class="contenuto">
-        <!--Contenuto della pagine qui sotto-->
+        <?php
+        echo "<h1>Operazioni utente</h1>";
+        echo "<h3>Seleziona utente </h3>";
+        $str = "<select name='nome'>";
+        $str .= "<option value='0'></option>";
+        $query = "SELECT id, mail FROM utente";
+        $conn = connettiDb();
+        $ris=$conn->query($query);
+        if ($ris->num_rows > 0) {
+            while($row = $ris->fetch_assoc()) {
+                $str .= "<option value='".$row["id"]."'>".$row["mail"]."</option>";
+            }
+        }
+        $str.="</select>";
+        echo $str;
+        ?>
+        <br>
+        <br>
+        <form action='gestisci_utente.php' method='post'>
+            <input type='submit' name='submit' value='Crea utente'>
+            <input type='submit' name='submit' value='Modifica utente'>
+            <input type='submit' name='submit' value='Elimina utente'>
+        </form>
+
+        <form action="gestisci_utente.php" method="post">
+            <br><br>
+            <h2>Crea utente</h2>
+            <label>Mail</label>
+            <input type="text" name="mail_inpt">
+            <label>Password</label>
+            <input type="text" name="pw_inpt"><br><br>
+            <label>Nome</label>
+            <input type="text" name="nome_inpt">
+            <label>Cognome</label>
+            <input type="text" name="cognome_inpt"><br><br>
+            <input type="submit" name="submit" value="Salva operazione">
+        </form>
+        <?php
+        echo "<h2>Gestione gruppi utente</h2><h3>Seleziona gruppo</h3>";
+        $str = "<select name='nome'>";
+        $str .= "<option value='0'></option>";
+        $query = "SELECT id, nome FROM gruppo";
+        $conn = connettiDb();
+        $ris=$conn->query($query);
+        if ($ris->num_rows > 0) {
+            while($row = $ris->fetch_assoc()) {
+                $str .= "<option value='".$row["id"]."'>".$row["nome"]."</option>";
+            }
+        }
+        $str.="</select>";
+        echo $str;
+
+        echo "<form action='gestisci_utente.php' method='post'>
+            <br><br>
+            <input type='submit' name='add_usr_to_grp' value='Aggiungi al gruppo'>
+            <input type='submit' name='rem_usr_frm_grp' value='Rimuovi dal gruppo'><br><br>
+        </form>";
+        echo "<h4>Gruppi già associati</h4>";
+        $mail_selected = "ciao@gmail.com";
+        echo "Mail: ".$mail_selected."<br>";
+        $conn = connettiDb();
+        $str = "<table style='border: 1px solid black'><tr style='border: 1px solid black'><th style='border: 1px solid black'>Nome</th></tr>";
+        $query = "SELECT nome FROM gruppo INNER JOIN appartienea app ON app.idGruppo = g.id WHERE app.idUtente = (SELECT id FROM utente WHERE mail = '$mail_selected')";
+        $conn = connettiDb();
+        echo $query;
+        $ris = $conn->query($query);
+        if ($ris->num_rows > 0) {
+            while($row = $ris->fetch_assoc()) {
+                $str .= "<tr style='border: 1px solid black'><td style='border: 1px solid black'>".$row["nome"]."</td></tr>";
+            }
+        }
+        $str.="</table>";
+        echo $str;
+        ?>
+
+
     </div>
 </div>
 </body>
