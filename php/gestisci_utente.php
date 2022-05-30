5f3c7e9eec136e0e5ae7fda8b9e26c3e046841df
Gabriele Groppo <gabriele.groppo@alessandrinimainardi.edu.it>
2022-05-30 11:13:29 +0200

    Creazione pagina gestisci utente

---
+++ php/gestisci_utente.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../stile/globalStyle.css">
+    <title>Gestisci Utente</title>
+</head>
+<body>
+<div class="container">
+    <div class="logo">
+        <img src="../immagini//logoScuola.png" alt="logo scuola" class="logo-scuola">
+    </div>
+    <div class="titolo">
+        <p class="titolo-header">Gestisci Utente</p>
+    </div>
+    <?PHP
+    include "Navbar.php";
+    ?>
+    <div class="contenuto">
+        <!--Contenuto della pagine qui sotto-->
+    </div>
+</div>
+</body>
+</html>
