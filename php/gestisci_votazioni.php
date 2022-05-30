b98f33f294d27116c6c2e11e39689425d440d9eb
DaniloFuse <101499173+DaniloFuse@users.noreply.github.com>
2022-05-30 11:51:01 +0200

    continuo pagina gestisci_votazione

--- php/gestisci_votazioni.php
+++ php/gestisci_votazioni.php
@@ -6,13 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../stile/globalStyle.css">
     <title>Votazione</title>
-    <script>
-        jQuery(document).ready(function($){
-            $(".clickable-row").click(function(){
-                window.location = $(this).data("href");
-            });
-        });
-    </script>
+
 </head>
 <body>
     <div class="container">
@@ -29,70 +23,89 @@
             <center>
             <h1 style="align:center; font-family: 'Roboto Mono', monospace;font-family: 'Space Mono', monospace;">Lista Votazioni</h1>
 			<?php
-				if(isset($_POST['crea']))  
-				{
-					echo "crea";
+			
+				$server = "localhost";
+				$username = "root";
+				$password = "";
+				$dbName = "votazioniscolastiche";
+				$sql = "";
+				$tab;
+
+				$conn = new mysqli($server, $username, $password, $dbName);
+
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
 				}
-				if(isset($_POST['modifica']))  
+				
+				if ($_SERVER["REQUEST_METHOD"] == "POST") 
 				{
-					echo "modifica";
-				}
-				if(isset($_POST['cancella']))  
-				{
-					echo "cancella";
+					if(isset($_POST['crea']))  
+					{
+						$content = "<form method='post'> 
+									Quesito:<input type='text' name='modifica' value='' required><br>
+									<select name='tipo'>	
+										<option name='tipo' value='anonimo'>anonimo</option>\n
+										<option name='tipo' value='nominale'>nominale</option>\n
+									</select><br>
+									Data e ora inizio:<input type='text' name='inizio' value=''required><br>
+									Data e ora fine:<input type='text' name='tipo' value=''required><br>
+									Numero di selte max:<input type='text' name='tipo' value=''required><br>
+									
+									<br><input type='submit' name='invia' value='salva'><br>
+
+									</form>";
+									
+									
+						echo $content;
+						
+						if($_POST['invia'] == 'salva'){
+							$sql = "Insert into votazione (quesito, tipo, inizio, fine, scelteMax) values ()";
+						
+						}
+						
+						if(isset($_POST['invia'])){
+							$query = "INSERT INTO votazione (quesito, tipo, inizio, fine, scelteMax) values ()";
+							$ris=$conn->query($query);
+						}
+						
+					}
+					if(isset($_POST['modifica']))  
+					{
+						echo "modifica";
+					}
+					if(isset($_POST['cancella']))  
+					{
+						echo "cancella";
+					}
+				}else{
+					$content = "<h3>OPERAZIONE VOTAZIONE </h3>
+								<br>
+						
+								<p>Nome della votazione</p>".
+						
+								'<form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '">'.
+							
+								"<select name='quesito'>";
+
+					$query = "SELECT  id, quesito FROM votazione /*ORDER BY */";
+					$ris=$conn->query($query);
+					if ($ris->num_rows > 0) {
+						while($row = $ris->fetch_assoc()) {
+							$quesito = $row["quesito"];
+							$id = $row["id"];
+							// $altro = $row["altro"];
+							$content.= "<option name='quesito' value='$id'>$quesito</option>\n";
+						}
+					}
+					$content.="</select><br><input type='submit' name='crea' value='Crea votazione'/><br>
+								<br><input type='submit' name='modifica' value='Modifica votazione'/><br>
+								<br><input type='submit' name='cancella' value='Cancella votazione'/><br>
+								<br><input type='submit' name='gestisci' value='Gestisci opzione'/><br>
+								</form>";
+					echo $content;
 				}
 			?>
 			
-		<?php
-			$server = "localhost";
-			$username = "root";
-			$password = "";
-			$dbName = "votazioniscolastiche";
-			$sql = "";
-			$tab;
-
-
-			$conn = new mysqli($server, $username, $password, $dbName);
-
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			}
-
-
-			$content = "<h3>OPERAZIONE VOTAZIONE </h3>
-						<br>
-						
-						<p>Nome della votazione</p>
-						
-						<form action = '', method = 'POST'> 
-							
-						<select name='quesito'>";
-
-			$query = "SELECT  id, quesito FROM votazione /*ORDER BY */";
-			$ris=$conn->query($query);
-			if ($ris->num_rows > 0) {
-				while($row = $ris->fetch_assoc()) {
-					$quesito = $row["quesito"];
-					$id = $row["id"];
-				// $altro = $row["altro"];
-				$content.= "<option value='$id'>$quesito</option>\n
-								</select>";
-			  }
-			}
-			echo $content;
-		?>
-		
-		<form action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method = 'POST'>
-			<br>
-			<br><input type="submit" name="crea" value="Crea votazione"/><br>
-			<br><input type="submit" name="modifica" value="Modifica votazione"/><br>
-			<br><input type="submit" name="cancella" value="Cancella votazione"/><br>
-		</form>
-		<form action='<?php echo htmlspecialchars('gestisci_opzione.php'); ?>' method = 'POST'>
-			<br><input type="submit" name="gestisci" value="Gestisci opzione"/><br>
-		</form>
-		
-		
 		</center>
         </div>
     </div>
