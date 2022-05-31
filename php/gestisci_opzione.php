6861f25f5b9086565541b9a0923a54294cb505de
Skenny <matteo170803@gmail.com>
2022-05-31 09:24:55 +0200

    Aggiunta cancellazione e modifica a gestisci_opzione

--- php/gestisci_opzione.php
+++ php/gestisci_opzione.php
@@ -21,16 +21,46 @@
         ?>
         <div class="contenuto">
             <!--Contenuto della pagine qui sotto-->
+
+
             <?php
                 //session_start();
                 $server = "localhost";
 			    $username = "root";
 			    $password = "";
 			    $dbName = "votazioniscolastiche";
-                $idVotazione = 1;
+                $idVotazione = 3;
 
                 if ($conn->connect_error) {
                     die("Connection failed: " . $conn->connect_error);
+                }
+
+                if($_SERVER["REQUEST_METHOD"] == "POST"){  
+                    if($_POST["operazione"] == 1){
+                        $opzione = $_POST['opzione'];
+                        //$idVotazione = $_POST['id'];
+                        $query = "insert into opzione(testo,idVotazione) values ('" . $opzione . "', '" . $idVotazione . "')";
+                        $conn->query($query);
+                        /*$result = $conn->query($query);
+                        $row = $result->fetch_assoc();*/
+                    
+                    }else if($_POST["operazione"] == 2){
+                        $testo = $_POST["testo"];
+                        $idOpzione = $_POST["idOperazione"];
+                        //echo "allert('" . $testo . "')";
+
+                        $query = "UPDATE opzione SET testo = '" . $testo . "' WHERE id = " . $idOpzione;
+                        $conn -> query($query);
+                    }else if($_POST["operazione"] == 3){
+                        $idOpzione = $_POST["idOperazione"];
+
+                        $query = "DELETE FROM opzione WHERE id = " . $idOpzione;
+                        $conn -> query($query);
+
+                    }
+                         //echo "ciaooooooooooo giggggiii";
+                   
+    
                 }
 
                 /*if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -43,40 +73,48 @@
 
                 $query = "select quesito from votazione WHERE id = " . $idVotazione;
                 $result = $conn->query($query);
-                $row = $result->fetch_assoc();
-                //$conn->close();
+                
+                
+                while($row = $result->fetch_assoc()){
+                    //$conn->close();
+                    echo "<center>
+                            <p class='titoli'>AGGIUNGI OPZIONI AL QUESITO:</p>
+                            <p class='titoli'>" . $row['quesito'] . "</p>
+                        </center>";
+                }
+                    echo '<form method="post" action= "' . htmlspecialchars($_SERVER["PHP_SELF"]).'">';
+                    echo "<center><input type='text' name = 'opzione'></center><br>";
+                    echo "<center><input type='hidden' name = 'operazione' value=1></center><br>";
+                    //echo "<center><input type='hidden' name='id' value=" . $idVotazione . "/></center>";
+                    echo "<center><input type='submit' name='crea' value='Aggiungi opzione'/></center>";
+                    echo "</form>";
+                    echo "<br><br><br><br><br>";
+                
 
-                echo "<center>
-                        <p class='titoli'>AGGIUNGI OPZIONI AL QUESITO:</p>
-                        <p class='titoli'>" . $row['quesito'] . "</p>
-                    </center>";
-                echo "<form method='POST' action='" . <?php
-                                                            $server = "localhost";
-                                                            $username = "root";
-                                                            $password = "";
-                                                            $dbName = "votazioniscolastiche";
-                                                            $idVotazione = 1;
+                $query = "select testo, id FROM opzione WHERE idVotazione = " . $idVotazione;
+                $result = $conn->query($query);
 
-                                                            if ($conn->connect_error) {
-                                                                die("Connection failed: " . $conn->connect_error);
-                                                            }
-
-                                                            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                                                                $opzione = $_SERVER["opzione"];
-                                                                $idVotazione = $_SERVER["id"];
-                                                            }
-
-                                                            
-
-
-                                                            
-                                                        ?> . "'>"
-                echo "<center><input type='text' name = 'opzione'></center><br>";
-                echo "<center><input type='hidden' name='id' value=" . $idVotazione . "/></center>";
-                echo "<center><input type='submit' name='crea' value='Aggiungi opzione'/></center>";
-                echo "</form>"
-            
+                while($row = $result->fetch_assoc()){
+                    echo "<center>";
+                    echo '<form method="post" action= "' . htmlspecialchars($_SERVER["PHP_SELF"]).'">';
+                    echo "<input type='text' name = 'testo' value='" . $row["testo"] . "'>";
+                    echo "<input type='hidden' name = 'operazione' value=2>";
+                    echo "<input type='hidden' name = 'idOperazione' value='" . $row["id"] . "'>";
+                    echo "<input type='submit' value='modifica'>";
+                    echo "</form>";
+                    //echo '</form>';
+                    echo '<form method="post" action= "' . htmlspecialchars($_SERVER["PHP_SELF"]).'">';
+                    echo "<input type='hidden' name = 'operazione' value=3>";
+                    echo "<input type='hidden' name = 'idOperazione' value='" . $row["id"] . "'>";
+                    echo "<input type='submit' value='cancella'>";
+                    echo '</form>';
+                    echo "</center>";
+                    echo "<br>";
+                }
+            //$conn->close();
             ?>
+
+
         </div>
     </div>
 </body>
