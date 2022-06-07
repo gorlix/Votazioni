80b71331f169a8ebb37af0162243eb5f571919b7
toneskater <toneskater@yahoo.it>
2022-06-07 12:07:42 +0200

    ...

--- php/home.php
+++ php/home.php
@@ -1,4 +1,10 @@
-
+<?php
+/**
+* @author skenny;Matteo Schintu
+* @author Daniele
+* @author Negro
+*/
+?>
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -7,13 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../stile/globalStyle.css">
     <title>Votazione</title>
-    <script>
-        jQuery(document).ready(function($){
-            $(".clickable-row").click(function(){
-                window.location = $(this).data("href");
-            });
-        });
-    </script>
+
 </head>
 <body>
     <div class="container">
@@ -30,14 +30,21 @@
             <center>
             <h1 style="align:center; font-family: 'Roboto Mono', monospace;font-family: 'Space Mono', monospace;">Lista Votazioni</h1>
         <?php
+        if(!isset($_SESSION)) { 
+            session_start(); 
+        }
+
         $server = "localhost";
         $username = "root";
         $password = "";
         $dbName = "votazioniscolastiche";
+
         $sql = "";
         $tab;
+        $idUtente = $_SESSION['id_utente'];
 
         $conn = mysqli_connect($server,$username,$password,$dbName);
+
         if(!$conn){
             die("Connessione Fallita: " . mysqli_connect_error());
         }
@@ -45,35 +52,50 @@
         $sql = "select id,quesito,tipo,fine FROM votazione";
 
         $result = mysqli_query($conn,$sql);
+
         $tab = "<table style='width:100%'>
                     <tr style='width:100%; border-bottom: 2px solid black'>
                         <td id='titoli'>TITOLO</td>
                         <td id='titoli'>TIPO</td>
                         <td id='titoli'>COMPLETATO</td>
+                        <td></td>
                     </tr>";
-        if(mysqli_query($conn,$sql)){
+
+        if(mysqli_num_rows($result) > 0){
             while($row=mysqli_fetch_assoc($result)){
-                $tab .= "<form method = 'post' action = 'votazione.php'>";
-                $tab .= "<tr class='clickable-row' style='width:100%' >
-                            <td name='id'>" . $row['quesito'] . "</td>
-                            <input type='hidden' name='id' value='".$row['id']."'>
-                            <td>" . $row['tipo'] . "</td>";
-                if($row['fine'] >= date("Y-M-D h:i:sa")){
-                    $tab .= "<td> SI </td>
-                        </tr> </form>";
-                }else{
-                    $tab .= "<td> NO </td>
-                        </tr> </form>";
+                $tab .= "<form method = 'GET' action = 'votazione.php'>";
+
+                $query2 = "select hash FROM esegue WHERE idUtente = " . $idUtente . " AND idVotazione = " . $row['id'];
+                
+                $result2 = mysqli_query($conn,$query2);
+                
+                $tab .= "<tr class='clickable-row' style='width:100%' >";
+            
+                if(mysqli_num_rows($result2) > 0){
+                    while($row2=mysqli_fetch_assoc($result2)){
+                        if($row2['hash'] != "")
+                            $tab .="<input type='hidden' name = 'hash' value = '" . $row2['hash'] . "'>";                  
+                    }
                 }
+                
+                $tab .= "<td name='id'>" . $row['quesito'] . "</td>
+                <input type='hidden' name='id' value='".$row['id']."'>
+                <td>" . $row['tipo'] . "</td>";
+                
+                if($row['fine'] >= date("Y-M-D h:i:sa")){
+                    $tab .= "<td> SI </td>";
+                }else{
+                    $tab .= "<td> NO </td>";
+                }
+
+                $tab .= "<td><input type='submit' class='button' value='vai a votazione'></td>";
+                $tab .= "</tr> </form>";
             }
         }
-
-        echo $tab;
-        
-
-        
+        echo $tab . "</table>";
     ?>
-    </center>
+        </center>
+        <p class="errore"><?php  if(isset($_GET['hash'])) {echo "Non hai i permessi per accedere a quetsa votazione";$_GET['hash'] = "";}?></p>
         </div>
     </div>
 </body>
