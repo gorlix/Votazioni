56618916b7d4012596843960f0f1c4152f1eea40
Skenny <matteo170803@gmail.com>
2022-06-18 00:43:39 +0200

    Foto Aggiornate

--- php/sviluppatori.php
+++ php/sviluppatori.php
@@ -29,40 +29,35 @@
         <div class="titolo">
             <p class="titolo-header">Sviluppatori</p>
         </div>
-        <!-- <?PHP
+         <?PHP
             include "Navbar.php";
-        ?> -->
+        ?> 
         <div class="contenuto">
             <center>
                 <table>
+                <tr>
+                        <td><img class='img' src="../foto/Tonello.jpg"></td>
+                        <td><h2>Andrea Tonello</h2><h3>Docente</h3></td>
+                    </tr>
                     <tr>
                         <td><img class='img' src="../foto/Schintu.jpg"></td>
                         <td><h2>Matteo Schintu</h2><h3>5Bi</h3><h3>Anno Scolastico: 2022-2023</h3></td>
-                    </tr>
-                    <tr>
                         <td><img class='img' src="../foto/Daniele.jpg"></td>
                         <td><h2>Cosimo Daniele</h2><h3>5Bi</h3><h3>Anno Scolastico: 2022-2023</h3></td>
                     </tr>
                     <tr>
                         <td><img class='img' src="../foto/Negro.jpg"></td>
                         <td><h2>Simone Negro</h2><h3>5Bi</h3><h3>Anno Scolastico: 2022-2023</h3></td>
-                    </tr>
-                    <tr>
                         <td><img class='img' src="../foto/Gorla.jpg"></td>
                         <td><h2>Alessandro Gorla</h2><h3>5Bi</h3><h3>Anno Scolastico: 2022-2023</h3></td>
                     </tr>
                     <tr>
                         <td><img class='img' src="../foto/Fusè.jpg"></td>
                         <td><h2>Danilo Fusè</h2><h3>5Bi</h3><h3>Anno Scolastico: 2022-2023</h3></td>
-                    </tr>
-                    <tr>
                         <td><img class='img' src="../foto/Groppo.jpg"></td>
                         <td><h2>Gabriele Groppo</h2><h3>5Bi</h3><h3>Anno Scolastico: 2022-2023</h3></td>
                     </tr>
-                    <tr>
-                        <td><img class='img' src="../foto/Tonello.jpg"></td>
-                        <td><h2>Andrea Tonello</h2><h3>Docente</h3></td>
-                    </tr>
+                    
                 </table>
             </center>
         <p class="errore"><?php  if(isset($_GET['hash'])) {echo "Non hai i permessi per accedere a quetsa votazione";$_GET['hash'] = "";}?></p>
